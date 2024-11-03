5cf8ec4a31e9e5b80db5540f6508d11546958823
tvirat <155598251+tvirat@users.noreply.github.com>
2024-11-03 14:07:32 -0500

    added submit button

--- frontend/my-react-app/src/components/RecommendationForm.jsx
+++ frontend/my-react-app/src/components/RecommendationForm.jsx
@@ -2,16 +2,31 @@ import React from "react";
 
 const RecommendationForm = () => {
   return (
-    <form>
-      <label htmlFor="movieName">
-        Type the movie name to get recommendations
+    <form id="recommendation-form">
+      <label htmlFor="directions">
+        Type the movie title in the text box and specify the number of
+        recommendations
       </label>
       <br />
+      <label htmlFor="movieName">Movie Title</label>
       <input type="text" name="movieInput" id="movieInput" />
       <br />
-      <button type="submit">Submit</button>
+      <label htmlFor="numOfRecs">Number of Recommendations: </label>
+      <input type="text" name="numOfRecs" id="numOfRecs" />
+      <br />
+      <button type="submit" onClick={retrieveTitle()} formMethod="post">
+        Submit
+      </button>
     </form>
   );
+};
+
+const retrieveTitle = () => {
+  const formData = document
+    .getElementById("recommendation-form")
+    .getElementsByTagName("input");
+  const title = formData[0];
+  const numOfRecs = formData[1];
 };
 
 export default RecommendationForm;
