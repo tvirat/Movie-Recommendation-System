63ed77ddd156f328cb415cbe6fcc93d5a7411a5b
tvirat <155598251+tvirat@users.noreply.github.com>
2024-11-10 10:31:39 -0500

    updated the api call url

--- frontend/src/components/RecommendationForm.jsx
+++ frontend/src/components/RecommendationForm.jsx
@@ -19,17 +19,14 @@ class RecommendationForm extends Component {
       },
       async () => {
         try {
-          const response = await fetch(
-            "http://127.0.0.1:5000/api-get-recommendations",
-            {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({
-                movieTitle: this.state.movieTitle,
-                numOfRecs: this.state.numRec,
-              }),
-            }
-          );
+          const response = await fetch("/api-get-recommendations", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              movieTitle: this.state.movieTitle,
+              numOfRecs: this.state.numRec,
+            }),
+          });
 
           // Check if response is ok before parsing JSON
           if (!response.ok) {
