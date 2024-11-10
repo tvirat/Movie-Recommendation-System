12165cf0a283ba6aeb8db5647e8942884a29d222
tvirat <155598251+tvirat@users.noreply.github.com>
2024-11-09 22:46:52 -0500

    added the function to handle api calls

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import notebook
 
 app = Flask(__name__)
 
-@app.route('/')
-def give_recommendations() -> list[str]:
-    return ['hi']
+@app.route('/api-get-recommendations', methods=['POST'])
+def give_recommendations(movie_title: str, num_of_rec) -> list[str]:
+    if isinstance(num_of_rec, str):
+        num_of_rec = int(num_of_rec)
+    
+    return notebook.get_top_matches(movie_title, num_of_rec)
 
 if __name__ == '__main__':
     app.run(debug=False)
