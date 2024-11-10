c71a73056ce4a2f44991b08fdfe012fb7dde910a
tvirat <155598251+tvirat@users.noreply.github.com>
2024-11-10 00:08:00 -0500

    debugged the api calls made to the function

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask
-import notebook
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import main
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api-get-recommendations', methods=['POST'])
-def give_recommendations(movie_title: str, num_of_rec) -> list[str]:
-    if isinstance(num_of_rec, str):
-        num_of_rec = int(num_of_rec)
-    
-    return notebook.get_top_matches(movie_title, num_of_rec)
-
+def get_recommendations():
+    data = request.get_json()
+    print(f"Received data: {data}")
+    try: 
+        movie_title = data.get('movieTitle') 
+        num_of_rec = data.get('numOfRecs') 
+        if isinstance(num_of_rec, str): 
+            num_of_rec = int(num_of_rec) 
+        
+        recommendations = main.get_top_matches(movie_title, num_of_rec) 
+        return jsonify(recommendations)
+    except AttributeError as e: 
+        print("Error: ", e) 
+        return jsonify({"error": "Internal server error"}), 500
 if __name__ == '__main__':
     app.run(debug=False)
