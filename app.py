7d060804ba8f75ad57f7861326297c26c2cc3aa9
tvirat <155598251+tvirat@users.noreply.github.com>
2024-11-09 21:45:48 -0500

    added the submit event handler

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def give_recommendations() -> list[str]:
-    return ['']
+    return ['hi']
 
 if __name__ == '__main__':
     app.run(debug=False)
--- frontend/my-react-app/src/components/RecommendationForm.jsx
+++ frontend/my-react-app/src/components/RecommendationForm.jsx
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 
-const RecommendationForm = () => {
-  return (
-    <form id="recommendation-form">
-      <label htmlFor="directions">
-        Type the movie title in the text box and specify the number of
-        recommendations
-      </label>
-      <br />
-      <label htmlFor="movieName">Movie Title</label>
-      <input type="text" name="movieInput" id="movieInput" />
-      <br />
-      <label htmlFor="numOfRecs">Number of Recommendations: </label>
-      <input type="text" name="numOfRecs" id="numOfRecs" />
-      <br />
-      <button type="submit" onClick={retrieveTitle()} formMethod="post">
-        Submit
-      </button>
-    </form>
-  );
-};
+class RecommendationForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movieTitle: "",
+      numRec: 3,
+    };
+  }
 
-const retrieveTitle = () => {
-  const formData = document
-    .getElementById("recommendation-form")
-    .getElementsByTagName("input");
-  const title = formData[0];
-  const numOfRecs = formData[1];
-};
+  getValues = () => {
+    const numOfRecs = document.getElementById("numOfRecs").value;
+    this.setState({
+      movieTitle: document.getElementById("movieInput").value,
+      numRec: numOfRecs || this.state.numRec,
+    });
+    Event.pre;
+  };
+
+  render() {
+    return (
+      <form id="recommendation-form">
+        <label htmlFor="directions">
+          Type the movie title in the text box and specify the number of
+          recommendations
+        </label>
+        <br />
+        <label htmlFor="movieName">Movie Title</label>
+        <input type="text" name="movieInput" id="movieInput" />
+        <br />
+        <label htmlFor="numOfRecs">Number of Recommendations: </label>
+        <input type="text" name="numOfRecs" id="numOfRecs" />
+        <br />
+        <button
+          id="submitButton"
+          type="submit"
+          onClick={() => this.getValues()}
+          onSubmit={() => event.preventDefault()}
+          formMethod="post"
+        >
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
 
 export default RecommendationForm;
